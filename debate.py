e710fb076bff613a18d7185009f3a534c2e4e81e
ncheng <nlscng.g@gmail.com>
2025-07-01 21:52:07 -0500

    Some init batch of files, and project def file.

---
+++ debate.py
@@ -0,0 +1,6 @@
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+
+model = OpenAIChatCompletionClient(
+    model="gpt-4o",
+    api_key="",
+)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from autogen-laughing-doodle!")
+
+
+if __name__ == "__main__":
+    main()
